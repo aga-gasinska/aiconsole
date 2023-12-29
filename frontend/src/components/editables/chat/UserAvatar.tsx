75e9e4c4ab5bee5d36d073ca30c5f5b6d2985abe
vlad <vlad@malinovski.org>
2023-12-29 19:32:24 +0100

    * feat(api): add ability to change username, email + gravatar integration

---
+++ frontend/src/components/editables/chat/UserAvatar.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import ky from 'ky';
+import { useAPIStore } from '@/store/useAPIStore';
+import { cn } from '@/utils/common/cn';
+
+interface UserAvatarProps {
+  email?: string;
+  title?: string;
+  type: 'large' | 'small' | 'extraSmall';
+  className?: string;
+}
+
+interface AvatarResponse {
+  avatar_url: string;
+  username: string;
+}
+
+export function UserAvatar({ email, title, type, className }: UserAvatarProps) {
+  const [avatarURL, setAvatarURL] = useState('');
+  const getBaseURL = useAPIStore((state) => state.getBaseURL);
+
+  useEffect(() => {
+    const fetchAvatar = async () => {
+      if (email) {
+        try {
+          const response = await ky.get(`${getBaseURL()}/profile`, { searchParams: { email } }).json<AvatarResponse>();
+          if (response.avatar_url) {
+            setAvatarURL(response.avatar_url);
+          }
+        } catch (error) {
+          console.error('Error fetching avatar URL:', error);
+        }
+      }
+    };
+
+    fetchAvatar();
+  }, [email, getBaseURL]);
+
+  return (
+    <img
+      title={title}
+      src={avatarURL}
+      className={cn(className, 'rounded-full mb-[10px] mt-[5px] border border-slate-800', {
+        'w-20 h-20 ': type === 'large',
+        'w-16 h-16': type === 'small',
+        'w-6 h-6': type === 'extraSmall',
+      })}
+    />
+  );
+}
