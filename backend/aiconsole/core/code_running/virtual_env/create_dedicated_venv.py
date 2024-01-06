14b730818eef77324ac4599a37b87088d554536d
vlad <vlad@malinovski.org>
2024-01-06 14:29:16 +0100

    * feat: update Python version from 3.10 to 3.11 * fix: handle pip install commands in python code interpreter * refactor: improve venv creation and dependency installation * fix: handle python download exceptions and switch from requests to urllib * refactor: update path handling for different platforms in env.py * fix: update bundled python version in electron app

--- backend/aiconsole/core/code_running/virtual_env/create_dedicated_venv.py
+++ backend/aiconsole/core/code_running/virtual_env/create_dedicated_venv.py
@@ -1,59 +1,78 @@
 import logging
+import shutil
 import subprocess
 import sys
 
+from aiconsole.consts import DIR_WITH_AICONSOLE_PACKAGE
 from aiconsole.core.code_running.virtual_env.install_and_upgrade_pip import (
     install_and_update_pip,
 )
 from aiconsole.core.code_running.virtual_env.install_dependencies import (
     install_dependencies,
 )
-from aiconsole_toolkit.env import get_current_project_venv_path
+from aiconsole_toolkit.env import (
+    get_current_project_user_packages,
+    get_current_project_venv_path,
+    get_current_project_venv_python_path,
+)
 
 _log = logging.getLogger(__name__)
 
 
 def run_subprocess(*args):
-    try:
-        process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
+    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
 
-        if process.returncode != 0:
-            _log.error(f"Command {' '.join(args)} failed with error: {stderr.decode().strip()}")
-            raise RuntimeError(stderr.decode().strip())
+    if process.returncode != 0:
+        _log.error(f"Command {' '.join(args)} failed with error: {stderr.decode().strip()}")
+        raise RuntimeError(stderr.decode().strip())
 
-        return stdout.decode().strip()
-    except subprocess.CalledProcessError as e:
-            _log.error(f"Error getting Python version: {e.stderr}")
-            return None
+    return stdout.decode().strip()
 
 
 def get_python_version(python_executable):
     """Returns the Python version for the given executable."""
-    return run_subprocess(python_executable, "-c", "import sys; print(sys.version)")
-    
+    try:
+        return run_subprocess(python_executable, "-c", "import sys; print(sys.version)")
+    except Exception as e:
+        _log.exception(f"Error: {e}")
+        return None
 
 
 async def create_dedicated_venv():
-    venv_path = get_current_project_venv_path()  # Ensure this function returns a Path object
+    venv_path = get_current_project_venv_path()
     system_python_version = get_python_version(sys.executable)
 
     if not venv_path.exists():
-        _log.info(f"Creating venv in {venv_path}")
-        run_subprocess(sys.executable, "-m", "venv", venv_path, "--system-site-packages")
+        _log.info(f"Creating venv in {venv_path}, using {sys.executable}")
+        run_subprocess(sys.executable, "-m", "venv", str(venv_path), "--system-site-packages")
     else:
         _log.info(f"Venv already exists in {venv_path}")
 
-        # Check Python version in venv
-        venv_python_executable = venv_path / ("Scripts" if sys.platform == "win32" else "bin") / "python"
-        venv_python_version = get_python_version(venv_python_executable)
+        venv_python_executable = str(get_current_project_venv_python_path())
 
-        if venv_python_version != system_python_version:
-            _log.info("Upgrading venv due to different Python version.")
-            run_subprocess(sys.executable, "-m", "venv", "--upgrade", venv_path)
-            _log.info("Venv upgraded for new Python version.")
+        venv_python_version = get_python_version(venv_python_executable)
+        if venv_python_version:
+            _log.info(f"Valid venv python executable")
         else:
-            _log.info("Venv upgrade not necessary, Python version matches.")
+            _log.info(f"Reconstructing venv due to the different Python version({system_python_version})")
+            shutil.rmtree(str(get_current_project_venv_path()))
+            _log.info(f"1) Deleted old env.")
+
+            run_subprocess(sys.executable, "-m", "venv", str(venv_path), "--system-site-packages")
+            _log.info(f"2) Created venv in {venv_path}, using {sys.executable}")
 
     install_and_update_pip(venv_path)
-    install_dependencies(venv_path)
+    install_dependencies(
+        python_path=get_current_project_venv_python_path(),
+        dependency_file=DIR_WITH_AICONSOLE_PACKAGE / "pyproject.toml",
+    )
+
+    if get_current_project_user_packages().exists():
+        install_dependencies(
+            python_path=get_current_project_venv_python_path(),
+            dependency_file=get_current_project_user_packages(),
+        )
+        _log.info(f"Installed user packages")
+    else:
+        _log.info(f"No user packages to install")
